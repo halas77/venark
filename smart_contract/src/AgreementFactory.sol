6a3fc1d370aabf70759c69da0a3d56534fc593b0
Dawit Mellese <dawitm777@gmail.com>
2025-03-18 17:14:23 +0300

    feat: integrate ClientRegistry into AgreementFactory for user onboarding validation

--- smart_contract/src/AgreementFactory.sol
+++ smart_contract/src/AgreementFactory.sol
@@ -3,17 +3,24 @@ pragma solidity ^0.8.20;
 import {Clones} from "../lib/openzeppelin-contracts/contracts/proxy/Clones.sol";
 import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {ServiceAgreement} from "./ServiceAgreement.sol";
+import {ClientRegistry} from "./ClientRegistry.sol";
 
 contract AgreementFactory {
     // state variables
     address public immutable template;
     address public dao;
+    ClientRegistry public clientRegistry;
+
+    // errors
+    error AgreementFactory__UserNotFound();
+
     // events
     event AgreementCreated(address indexed client, address agreement);
 
     // initialization
     constructor(address _dao) {
         dao = _dao;
+        clientRegistry = new ClientRegistry();
         template = address(new ServiceAgreement(_dao));
     }
 
@@ -22,15 +29,13 @@ contract AgreementFactory {
         address client,
         ERC20 paymentToken,
         uint256 budget
-        // string calldata deliverablesCID
     ) external returns (address) {
+        if (!clientRegistry.isOnboarded(msg.sender)) {
+            revert AgreementFactory__UserNotFound();
+        }
         address clone = Clones.clone(template);
 
-        ServiceAgreement(clone).initialize(
-            client,
-            paymentToken,
-            budget
-        );
+        ServiceAgreement(clone).initialize(client, paymentToken, budget);
 
         emit AgreementCreated(client, clone);
         return clone;
