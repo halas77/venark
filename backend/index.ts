f16ac37abaff4d19034924d0d75f93f5f912ae37
Dawit Mellese <dawitm777@gmail.com>
2025-03-20 16:16:02 +0300

    feat: set up backend with Express, add example route and controller, and configure environment

---
+++ backend/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import exampleRoutes from "./routes/example.r";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Routes
+app.use("/api", exampleRoutes);
+
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`⚡ Server is running on http://localhost:${PORT}`);
+});
