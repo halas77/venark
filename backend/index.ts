1faca2405e913b6f8a3653cc2623d80309193a84
Dawit Mellese <dawitm777@gmail.com>
2025-04-08 14:08:24 +0300

    refactor: streamline dotenv configuration and remove unused imports delete: remove unused meme image files from public directory

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,10 @@
+require("dotenv").config();
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import exampleRoutes from "./routes/example.r";
 import creativeRoutes from "./routes/creative.r";
 import analyzerRoutes from "./routes/analyze.r";
-import agreementRoute from "./routes/agreement.r"
-
-dotenv.config();
+import agreementRoute from "./routes/agreement.r";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -20,7 +18,6 @@ app.use("/api/example", exampleRoutes);
 app.use("/api/generate-content", creativeRoutes);
 app.use("/api/analyze-content", analyzerRoutes);
 app.use("/api/create-agreement", agreementRoute);
-
 
 // Start Server
 app.listen(PORT, () => {
